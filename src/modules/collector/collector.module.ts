f8b693449c8d38446adf91d7a27048d1f8bb7835
Ryan Waldon <ryanpwaldon@gmail.com>
2021-09-28 21:58:32 +1000

    feat: updates

--- src/modules/collector/collector.module.ts
+++ src/modules/collector/collector.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { CollectorService } from './collector.service'
+import { CollectorCron } from 'src/modules/collector/collector.cron'
 import { AirtableModule } from 'src/modules/airtable/airtable.module'
 
 @Module({
   imports: [AirtableModule],
-  providers: [CollectorService],
+  providers: [CollectorService, CollectorCron],
 })
 export class CollectorModule {}
--- src/modules/collector/collector.service.ts
+++ src/modules/collector/collector.service.ts
@@ -1,21 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { Collector } from '../../types/Collector'
-import { Cron, CronExpression } from '@nestjs/schedule'
-import { COLLECTORS_TABLE_NAME } from 'src/constants/Airtable'
-import { AirtableService } from 'src/modules/airtable/airtable.service'
 
 @Injectable()
 export class CollectorService {
-  collectors: Collector[] = []
+  private collectors: Collector[] = []
 
-  constructor(private readonly airtableService: AirtableService) {}
-
-  @Cron(CronExpression.EVERY_10_SECONDS)
-  async updateAll() {
-    this.collectors = (await this.airtableService.findAll(COLLECTORS_TABLE_NAME)) as Collector[]
+  update(collectors: Collector[]) {
+    this.collectors = collectors
   }
 
-  async findAll() {
+  getCollectors() {
     return this.collectors
   }
 }
